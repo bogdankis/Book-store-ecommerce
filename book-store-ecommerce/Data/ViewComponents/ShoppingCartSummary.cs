e9713772fa0132ea54d3817f0292f1c3aba80265
bogdankis <chismbogdan@gmail.com>
2023-02-27 22:58:48 +0200

    added view component for shopping cart icon

---
+++ book-store-ecommerce/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,23 @@
+﻿using book_store_ecommerce.Data.Cart;
+using Microsoft.AspNetCore.Mvc;
+
+namespace book_store_ecommerce.Data.ViewComponents
+{
+    
+    public class ShoppingCartSummary: ViewComponent
+    {
+
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            return View(items.Count);
+        }
+    }
+}
