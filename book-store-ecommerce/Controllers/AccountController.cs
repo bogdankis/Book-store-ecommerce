053795197b49c1a4b4524632edcb3bde26bf3a6c
bogdankis <chismbogdan@gmail.com>
2023-04-15 19:04:01 +0300

    addded account controller

---
+++ book-store-ecommerce/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using book_store_ecommerce.Data;
+using book_store_ecommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace book_store_ecommerce.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, AppDbContext context) 
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
