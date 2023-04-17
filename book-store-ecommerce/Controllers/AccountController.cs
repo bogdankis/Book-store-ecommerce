cf7a59780673228b5f191c7b24406f0848e217c9
bogdankis <chismbogdan@gmail.com>
2023-04-17 16:55:45 +0300

    sign up user functionality

--- book-store-ecommerce/Controllers/AccountController.cs
+++ book-store-ecommerce/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using book_store_ecommerce.Data;
+using book_store_ecommerce.Data.Static;
 using book_store_ecommerce.Data.ViewModels;
 using book_store_ecommerce.Models;
 using Microsoft.AspNetCore.Identity;
@@ -59,6 +60,35 @@ namespace book_store_ecommerce.Controllers
         {
             var response = new RegisterVM();
             return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if(!ModelState.IsValid) 
+            {
+                return View(registerVM);
+            }
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if (user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+
+            }
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+            if (newUserResponse.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            }
+            return View("RegisterCompleted");
         }
     }
 }
